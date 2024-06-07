07963da85ce3802235804161a114cf482d779872
Adrián Rodríguez <Adrian.Rodriguez@indivasaas.com>
2024-06-08 01:05:27 +0200

    Cambios app.use cors 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { Server } = require("socket.io");
 
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-const args = process.argv.slice(2); 
+const args = process.argv.slice(2);
 
 const modelo = require("./app/servidor/modelo.js");
 
@@ -17,237 +17,172 @@ let io = new Server();
 
 const PORT = process.env.PORT || 3000;
 
-const haIniciado = function(request,response,next)
-{
-    let token = request.headers.authorization.split(' ')[1];
+const haIniciado = function(request, response, next) {
+    let token = request.headers.authorization && request.headers.authorization.split(' ')[1];
 
-    if (token)
-    {
+    if (token) {
         jwt.verify(token, "token", (err, decoded) => {
-            if (decoded)
-            {
+            if (decoded) {
                 next();
-            }
-            else
-            {
+            } else {
                 response.redirect("/")
             }
         });
+    } else {
+        response.redirect("/")
     }
 }
-
 
 const corsOptions = {
     origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     credentials: true,
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
 
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.sendStatus(200);
-});
-
-
-// app.use(cors({
-//     origin: '*',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//     credentials: true,
-//     optionsSuccessStatus: 204
-// }));
-
-// app.options('*', cors(corsOptions));
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-//     res.header("Access-Control-Allow-Credentials", true);
-//     next();
-// });
-
-
-let test=false; 
-test=eval(args[0]); //test=true test=false
+app.options('*', cors(corsOptions));
 
 app.use(express.static(__dirname + "/"));
 app.use(cookieSession({
     name: 'TFG',
     keys: ['key1', 'key2']
 }));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+let test = false;
+test = eval(args[0]); // test=true test=false
 
 let sistema = new modelo.Sistema(test);
 
-app.get("/good", function(request, response)
-{
-    let email=request.user.emails[0].value;
-    sistema.usuarioGoogle({"email": email}, function(usr)
-    {
-        response.cookie('email',usr.email);
+app.get("/good", function(request, response) {
+    let email = request.user.emails[0].value;
+    sistema.usuarioGoogle({ "email": email }, function(usr) {
+        response.cookie('email', usr.email);
         response.redirect('/');
     });
 });
-app.get("/fallo",function(request, response)
-{
-    response.send({email:"nook"})
+
+app.get("/fallo", function(request, response) {
+    response.send({ email: "nook" })
 });
 
-app.post("/registrarUsuario",function(request, response)
-{
-    sistema.registrarUsuario(request.body,function(res)
-    {
-        response.json({"email":res.email});
+app.post("/registrarUsuario", function(request, response) {
+    sistema.registrarUsuario(request.body, function(res) {
+        response.json({ "email": res.email });
     });
 });
 
-app.post("/agregarGoogleUser", function(request, response)
-{
-    sistema.usuarioGoogle(request.body,function(res)
-    {
-        response.json({"email": res.email, "token":res.token});
+app.post("/agregarGoogleUser", function(request, response) {
+    sistema.usuarioGoogle(request.body, function(res) {
+        response.json({ "email": res.email, "token": res.token });
     });
 });
 
-app.post("/agregarOpenAIUser", function(request, response)
-{
-    sistema.usuarioOpenAI(request.body,function(res)
-    {
-        response.json({"email": res.email, "token":res.token});
+app.post("/agregarOpenAIUser", function(request, response) {
+    sistema.usuarioOpenAI(request.body, function(res) {
+        response.json({ "email": res.email, "token": res.token });
     });
 });
 
-
-app.post("/loginUsuario",function(request,response)
-{
+app.post("/loginUsuario", function(request, response) {
     response.set('Access-Control-Allow-Credentials', true);
-    sistema.loginUsuarioEmail(request.body, function(res1)
-    {
-        if (res1.clave != -1)
-        {
-            response.json({"clave":res1.email, "token":res1.token});
-        }
-        else
-            sistema.loginUsuarioUsername(request.body, function(res2)
-            {
-                response.json({"clave":res2.email, "token":res2.token});                
+    sistema.loginUsuarioEmail(request.body, function(res1) {
+        if (res1.clave != -1) {
+            response.json({ "clave": res1.email, "token": res1.token });
+        } else {
+            sistema.loginUsuarioUsername(request.body, function(res2) {
+                response.json({ "clave": res2.email, "token": res2.token });
             });
+        }
     });
 });
 
-app.post("/addRecord/:email", haIniciado, function(request,response)
-{
-    let email=request.params.email;
-    sistema.addRecord(request.body, email, function()
-    {
-        response.json({"Correcto": true});
+app.post("/addRecord/:email", haIniciado, function(request, response) {
+    let email = request.params.email;
+    sistema.addRecord(request.body, email, function() {
+        response.json({ "Correcto": true });
     });
 });
 
+app.get("/", function(request, response) { });
 
-
-app.get("/", function(request,response)
-{
-    
-});
-
-app.get("/cierre", function(request, response)
-{
-    var contenido = fs.readFileSync(__dirname+"/app/servidor/cierre.html");
-    response.setHeader("Content-type","text/html");
+app.get("/cierre", function(request, response) {
+    var contenido = fs.readFileSync(__dirname + "/app/servidor/cierre.html");
+    response.setHeader("Content-type", "text/html");
     response.send(contenido);
 });
 
-app.get("/eliminarUsuario/:email", haIniciado, function(request,response)
-{
-    let email=request.params.email;
-    let res=sistema.eliminarUsuario(email);
+app.get("/eliminarUsuario/:email", haIniciado, function(request, response) {
+    let email = request.params.email;
+    let res = sistema.eliminarUsuario(email);
     response.send(res);
 });
 
-app.get("/comprobarUsuario/:email", haIniciado, function(request,response)
-{
-    let email=request.params.email;
+app.get("/comprobarUsuario/:email", haIniciado, function(request, response) {
+    let email = request.params.email;
 
-    if (sistema.usuarios[email])
-    {
-      response.send({"email": email});
+    if (sistema.usuarios[email]) {
+        response.send({ "email": email });
+    } else {
+        response.send({ "email": -1 });
     }
-    else
-    {
-      response.send({"email": -1});
-    }
-  })
+});
 
-app.get("/obtenerUsuario/:email", haIniciado, function(request, response)
-{
+app.get("/obtenerUsuario/:email", haIniciado, function(request, response) {
     let email = request.params.email;
     console.log(email);
-    sistema.obtenerUsuario({"email": email}, function(lista)
-    {
+    sistema.obtenerUsuario({ "email": email }, function(lista) {
         response.send(lista);
     })
-});  
+});
 
-app.put("/actualizarUsuario/:email", haIniciado, function(request, response) 
-{
+app.put("/actualizarUsuario/:email", haIniciado, function(request, response) {
     let email = request.params.email;
     let userProfile = request.body;
 
-    sistema.actualizarUsuario(email, userProfile, function(res) 
-    {
-        if (res.email !== -1) 
-            response.json({"Correcto": true});
-        else 
-            response.status(400).json({"Correcto": false});
+    sistema.actualizarUsuario(email, userProfile, function(res) {
+        if (res.email !== -1)
+            response.json({ "Correcto": true });
+        else
+            response.status(400).json({ "Correcto": false });
     });
 });
 
-app.get("/confirmarUsuario/:email/:key", function(request, response)
-{
+app.get("/confirmarUsuario/:email/:key", function(request, response) {
     let email = request.params.email;
     let key = request.params.key;
-    console.log({"email":email,"key":key})
-    sistema.confirmarUsuario({"email":email, "key":key}, function(usr)
-    {
-        if(usr.email!=-1)  
+    console.log({ "email": email, "key": key })
+    sistema.confirmarUsuario({ "email": email, "key": key }, function(usr) {
+        if (usr.email != -1)
             response.cookie('email', usr.email);
         response.redirect('/cierre');
     })
 })
 
-app.post('/enviarJwt', function(request, response)
-{
-    let jwt=request.body.jwt;
-    let user=JSON.parse(atob(jwt.split(".")[1]));
-    let email=user.email;
-    sistema.usuarioGoogle({"email":email}, function(obj)
-    {
-        response.send({'email':obj.email});
+app.post('/enviarJwt', function(request, response) {
+    let jwt = request.body.jwt;
+    let user = JSON.parse(atob(jwt.split(".")[1]));
+    let email = user.email;
+    sistema.usuarioGoogle({ "email": email }, function(obj) {
+        response.send({ 'email': obj.email });
     })
 });
 
-app.get("/cerrarSesion/:email", haIniciado, function(request,response)
-{
-    let email=request.params.email;
+app.get("/cerrarSesion/:email", haIniciado, function(request, response) {
+    let email = request.params.email;
     console.log(email)
-    if (email)
-    {
+    if (email) {
         sistema.eliminarUsuario(email);
     }
 });
 
-
-
 httpServer.listen(PORT, () => {
     console.log(`La aplicación se encuentra en el puerto: ${PORT}`);
-    console.log('Ctrl+C para terminar ejecucción');
+    console.log('Ctrl+C para terminar ejecución');
 });
 
 io.listen(httpServer);
-
-    
