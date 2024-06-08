317dfe19f3a10e2a9eb5cecdecdc720091f68c7f
Adrián Rodríguez <Adrian.Rodriguez@indivasaas.com>
2024-06-08 13:46:14 +0200

    Mirar configuracion total 4

--- index.js
+++ index.js
@@ -46,12 +46,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options('/*', function(req, res, next) {
+app.options('/*', cors(), function(req, res, next) {
 //  res.setHeader("Access-Control-Allow-Origin", "*");
 //  res.setHeader("Access-Control-Allow-Credentials", "false");
 //  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 //  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
  res.sendStatus(200);
  console.log(res);
 
